ae630558377ef5b0bbb9d9345d8fc7fe9389fa62
Professor108 <panstepan13@i.ua>
2021-12-24 23:30:40 +0200

    Add parse

--- engine/engine.go
+++ engine/engine.go
@@ -1,57 +1,80 @@
 package engine
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 type IHandler interface {
-  Post(cmd Command)
+	Post(cmd Command)
 }
 
 type Handler struct {
-  eventLoop *EventLoop
+	eventLoop *EventLoop
 }
 
 type Command interface {
-  Execute(handler IHandler)
+	Execute(handler IHandler)
 }
 
 type PrintCmd struct {
-  Msg string
+	Msg string
 }
 
 func (pc PrintCmd) Execute(handler IHandler) {
-  fmt.Println(pc.Msg)
+	fmt.Println(pc.Msg)
 }
 
 type DeleteCmd struct {
-  Str    string
-  Symbol byte
+	Str    string
+	Symbol byte
 }
 
 func (dc DeleteCmd) Execute(handler IHandler) {
-  r := strings.ReplaceAll(string(dc.Str), string(dc.Symbol), "")
-  var printCmd Command = &PrintCmd{r}
-  handler.Post(printCmd)
+	r := strings.ReplaceAll(string(dc.Str), string(dc.Symbol), "")
+	var printCmd Command = &PrintCmd{r}
+	handler.Post(printCmd)
 }
 
 type EventLoop struct {
-  queue []Command
+	queue []Command
 }
 
 func (eventLoop *EventLoop) Post(cmd Command) {
-  eventLoop.queue = append(eventLoop.queue, cmd)
+	eventLoop.queue = append(eventLoop.queue, cmd)
 }
 
 func (eventLoop *EventLoop) Start() {
-  eventLoop.queue = make([]Command, 0)
+	eventLoop.queue = make([]Command, 0)
 }
 
 func (eventLoop *EventLoop) AwaitFinish() {
-  for len(eventLoop.queue) > 0 {
-    cmd := eventLoop.queue[0]
-    eventLoop.queue = eventLoop.queue[1:]
-    cmd.Execute(eventLoop)
-  }
+	for len(eventLoop.queue) > 0 {
+		cmd := eventLoop.queue[0]
+		eventLoop.queue = eventLoop.queue[1:]
+		cmd.Execute(eventLoop)
+	}
+}
+
+func Parse(str string) Command {
+	array := strings.Fields(str)
+	if len(array) == 0 {
+		return &PrintCmd{Msg: "Line cannot be empty!!!"}
+	}
+	if array[0] == "delete" {
+		if len(array) == 3 {
+			byteArray := []byte(array[2])
+			if len(byteArray) == 1 {
+				return &DeleteCmd{Str: array[1], Symbol: byteArray[2]}
+			}
+			return &PrintCmd{Msg: "Must be 1 char you want to remove!!!"}
+		}
+		return &PrintCmd{Msg: "Must be 3 arguments: command, string which you wanna change and char!!!"}
+	} else if array[0] == "print" {
+		if len(array) == 2 {
+			return &PrintCmd{Msg: array[1]}
+		}
+		return &PrintCmd{Msg: "Must be 2 arguments: command and string which you wanna print!!!"}
+	}
+	return &PrintCmd{Msg: "Wrong command name. Exists only delete and print commands!!!"}
 }
